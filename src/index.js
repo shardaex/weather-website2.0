064c2753fd850cdc3895df221c33a6e0e5e854c4
shardaex <125917245+shardaex@users.noreply.github.com>
2023-04-07 15:29:18 +0200

    Changed cloud cover to wind and added id to humidity and wind

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+function formatDate(date) {
+  let hours = date.GetHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.GetMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.GetDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("input#search-input");
